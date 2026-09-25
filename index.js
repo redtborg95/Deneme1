05da486d6ea5678364843641ab5a0b57ef7b3d7b
redtborg95 <redtborg95@gmail.com>
2026-09-25 21:35:34 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+  host: 'BURAYA_SUNUCU_IP_YAZ.play.hosting',
+  port: 25565,
+  auth: 'offline',
+  version: false,
+  username: 'KapanmasinBotu'
+});
+
+bot.on('spawn', () => {
+  console.log('Bot oyuna basariyla girdi!');
+  
+  setInterval(() => {
+    bot.setControlState('jump', true);
+    setTimeout(() => bot.setControlState('jump', false), 500);
+    
+    const yaw = Math.random() * Math.PI * 2;
+    const pitch = (Math.random() * Math.PI) - (Math.PI / 2);
+    bot.look(yaw, pitch, true);
+  }, 180000);
+});
+
+bot.on('kicked', (reason) => {
+  console.log('Bottan atildi:', reason);
+});
+
+bot.on('error', (err) => {
+  console.log('Hata olustu:', err);
+});
