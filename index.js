503304023540975e1fa5df4cfc987d1c6aa052c8
redtborg95 <redtborg95@gmail.com>
2026-09-25 21:44:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const mineflayer = require('mineflayer');
 
-const bot = mineflayer.createBot({
-  host: 'lowednetwork.play.hosting',
-  port: 25565,
-  auth: 'offline',
-  version: "1.21.1", // Buraya sunucunun tam sürümünü yaz (Örn: 1.16.5, 1.20.1 vb.)
-  username: 'KapanmasinBotu'
-});
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'lowednetwork.play.hosting',
+    port: 25565,
+    auth: 'offline',
+    version: "1.21.1",
+    username: 'LOWEDNW'
+  });
 
-bot.on('spawn', () => {
-  console.log('Bot oyuna basariyla girdi!');
-  
-  setInterval(() => {
-    bot.setControlState('jump', true);
-    setTimeout(() => bot.setControlState('jump', false), 500);
+  bot.on('spawn', () => {
+    console.log('Bot oyuna basariyla girdi!');
     
-    const yaw = Math.random() * Math.PI * 2;
-    const pitch = (Math.random() * Math.PI) - (Math.PI / 2);
-    bot.look(yaw, pitch, true);
-  }, 180000);
-});
+    // Her 3 dakikada bir zıplama ve etrafa bakma hareketi (AFK kalmamak için)
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      setTimeout(() => bot.setControlState('jump', false), 500);
+      
+      const yaw = Math.random() * Math.PI * 2;
+      const pitch = (Math.random() * Math.PI) - (Math.PI / 2);
+      bot.look(yaw, pitch, true);
+    }, 180000);
+  });
 
-bot.on('kicked', (reason) => {
-  console.log('Bottan atildi:', reason);
-});
+  bot.on('kicked', (reason) => {
+    console.log('Bottan atildi:', reason);
+  });
 
-bot.on('error', (err) => {
-  console.log('Hata olustu:', err);
-});
+  bot.on('error', (err) => {
+    console.log('Hata olustu:', err);
+  });
+
+  // Oyundan düşerse veya atılırsa 5 saniye sonra tekrar bağlanması için kritik özellik:
+  bot.on('end', () => {
+    console.log('Bot oyundan dustu, 5 saniye sonra tekrar baglaniliyor...');
+    setTimeout(createBot, 5000);
+  });
+}
+
+createBot();
